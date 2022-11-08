6d38473d5cefcaa3b31fc601b626598ff076f2c9
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-08 14:31:00 +0300

    chore(test): use mdi/js and new styles

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <v-icon :path="mdiCheck" dense color="yellowgreen"></v-icon>
-    <v-icon :path="mdiClose" large color="red" disabled></v-icon>
+    <v-icon :path="mdiCheck" dense rotate="22" color="green" />
+    <v-icon :path="mdiClose" large color="red" />
+    <v-icon :path="mdiMagnify" rotate="32" color="blue" />
   </div>
 </template>
 
 <script>
-import { mdiCheck, mdiClose } from "./list";
+  import { mdiCheck, mdiClose } from "./list";
+  import { mdiMagnify } from "@mdi/js";
 
-export default {
-  setup() {
-    return {
-      mdiCheck,
-      mdiClose,
-    };
-  },
-};
+  export default {
+    setup() {
+      return {
+        mdiCheck,
+        mdiClose,
+        mdiMagnify,
+      };
+    },
+  };
 </script>
