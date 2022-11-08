93511b4d6188e89f1feb2362319dd49f8f1f1dc7
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-11-08 12:36:07 +0300

    chore: reorder properties and add required

--- src/components/icon.vue
+++ src/components/icon.vue
@@ -38,96 +38,97 @@
 </template>
 
 <script>
-export default {
-  name: "v-icon",
-  props: {
-    path: {
-      type: String,
-      default: "",
+  export default {
+    name: "v-icon",
+    props: {
+      color: {
+        type: String,
+        default: "currentColor",
+      },
+      dense: {
+        type: Boolean,
+        default: false,
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      large: {
+        type: Boolean,
+        default: false,
+      },
+      path: {
+        type: String,
+        default: "",
+        required: true,
+      },
+      rotate: {
+        type: [String, Number],
+        default: 0,
+      },
+      size: {
+        type: [String, Number],
+        default: 24,
+      },
+      small: {
+        type: Boolean,
+        default: false,
+      },
+      title: {
+        type: String,
+        default: "",
+      },
+      xLarge: {
+        type: Boolean,
+        default: false,
+      },
+      xSmall: {
+        type: Boolean,
+        default: false,
+      },
     },
-    title: {
-      type: String,
-      default: "",
-    },
-    color: {
-      type: String,
-      default: "currentColor",
-    },
-    xSmall: {
-      type: Boolean,
-      default: false,
-    },
-    small: {
-      type: Boolean,
-      default: false,
-    },
-    dense: {
-      type: Boolean,
-      default: false,
-    },
-    size: {
-      type: [String, Number],
-      default: 24,
-    },
-    large: {
-      type: Boolean,
-      default: false,
-    },
-    xLarge: {
-      type: Boolean,
-      default: false,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    rotate: {
-      type: [String, Number],
-      default: 0,
-    },
-  },
-  lib: {},
-  add(icons) {
-    if (typeof icons === "object" && icons !== null) {
-      this.lib = icons;
-    }
-  },
-  computed: {
-    icon() {
-      if (this.path) {
-        return this.path;
+    lib: {},
+    add(icons) {
+      if (typeof icons === "object" && icons !== null) {
+        this.lib = icons;
       }
-
-      return this.toCamelCase(this.path);
-
-      // const icon = this.$options.lib[this.toCamelCase(this.name)];
-
-      // if (typeof icon === "undefined") {
-      //   console.error(`[${this.name}] Name of the icon is incorrect`);
-      //   return;
-      // }
-      // return icon;
     },
-    transform() {
-      return this.rotate ? `transform: rotate(${this.rotate}deg)` : null;
-    },
-  },
-  methods: {
-    toCamelCase(str) {
-      const result = str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
-        return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
-      });
+    computed: {
+      icon() {
+        if (this.path) {
+          return this.path;
+        }
 
-      return result.replace(/\s+|[-]/g, "");
+        return this.toCamelCase(this.path);
+
+        // const icon = this.$options.lib[this.toCamelCase(this.name)];
+
+        // if (typeof icon === "undefined") {
+        //   console.error(`[${this.name}] Name of the icon is incorrect`);
+        //   return;
+        // }
+        // return icon;
+      },
+      transform() {
+        return this.rotate ? `transform: rotate(${this.rotate}deg)` : null;
+      },
     },
-  },
-};
+    methods: {
+      toCamelCase(str) {
+        const result = str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
+          return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
+        });
+
+        return result.replace(/\s+|[-]/g, "");
+      },
+    },
+  };
 </script>
 
 <style>
-.v-icon--disabled {
-  pointer-events: none;
-  opacity: 0.38;
-  fill: currentColor;
-}
+  .v-icon--disabled {
+    pointer-events: none;
+    opacity: 0.38;
+    fill: currentColor;
+  }
 </style>
